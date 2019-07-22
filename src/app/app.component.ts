f69afec7fe4186d3a075601e8b0af70ca3f44faa
ParulPetal <parulsingh550@gmail.com>
2019-07-22 22:45:52 +0530

    meassage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'APi';
+ 
+ title: string = '';
+  publisher;
+  image_url;
+
+  info: any;
+  API_KEY = "2dd5258613e19417fb9cb6b53dfa967b";
+  httpdata: any;
+ 
+ 
+  constructor(private http: HttpClient, _router:Router) { 
+
+    
+  }
+
+  onNameKeyUp(event: any) {
+    this.title = event.target.value;
+
+  }
+
+  getReceipe() {
+
+    this.http.get(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${this.API_KEY}&q=${this.title}&count=10`)
+      .subscribe((data) => { this.displaydata(data); }
+      )
+  }
+  displaydata(data) {
+  this.httpdata = data.recipes;
 }
+
+
+
+}
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+ 
+
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
